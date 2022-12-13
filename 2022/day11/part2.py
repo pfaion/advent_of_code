bddffd713352a5162a37177b52d16d2dbbd40747
Patrick Faion <p.faion@web.de>
2022-12-13 16:00:13 +0100

    Speedup 2022/11/2 to sub 1s by avoiding eval

--- 2022/day11/part2.py
+++ 2022/day11/part2.py
@@ -1,6 +1,7 @@
 import re
 from math import prod
 from pathlib import Path
+from typing import Callable
 
 data_raw = Path(__file__).with_name("input.txt").read_text()
 
@@ -9,8 +10,20 @@ items = [
     [int(item) for item in match.group("items").split(", ")]
     for match in re.finditer(r"items: (?P<items>(\d| |,)+)\n", data_raw)
 ]
+
+
+def parse_operation(text: str) -> Callable[[int], int]:
+    _, operator_string, operand_string = text.split()
+    operator = {"*": prod, "+": sum}[operator_string]
+    if operand_string == "old":
+        return lambda x: operator((x, x))
+    else:
+        operand = int(operand_string)
+        return lambda x: operator((x, operand))
+
+
 operations = [
-    match.group("operation")
+    parse_operation(match.group("operation"))
     for match in re.finditer(r"Operation: new = (?P<operation>.*)\n", data_raw)
 ]
 divisibility = [
@@ -23,12 +36,13 @@ next_monkey = [
 ]
 activities = [0 for _ in range(n_monkeys)]
 
+
 for round in range(10000):
     for monkey_i in range(n_monkeys):
         while len(items[monkey_i]) > 0:
             activities[monkey_i] += 1
             old = items[monkey_i].pop(0)
-            new = eval(operations[monkey_i])
+            new = operations[monkey_i](old)
             # I don't fully understand this, but I guess taking the modulus of
             # the product of all divisors will keep the operations stable across
             # all divisibility checks but keeps the numbers small. This was a
